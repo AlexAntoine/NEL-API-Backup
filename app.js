bdf297ee3a32081f36ef425448c35f0b0b12f750
Alex Antoine <antoinealex1234@gmail.com>
2023-04-26 14:36:29 -0400

    implement middleware

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv');
 const colors = require('colors');
 const express = require('express');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helment');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
 const {localDb,prodDb} = require('./db/devices');
 
 const app = express();
@@ -14,6 +19,21 @@ const deviceAge = require('./routes/deviceAge');
 app.use(express.json());
 
 prodDb();
+
+//Sanitize Data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helemt());
+
+//Prevent XSS attacksd
+app.use(xss());
+
+//Prevent http param pollution
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
 
 app.use('/api/current',deviceRouter);
 app.use('/api/old',oldRouter);
