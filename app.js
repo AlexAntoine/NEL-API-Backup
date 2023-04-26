8f5f2f5aff7d7a965423c1be75626bce3653d659
Alex Antoine <antoinealex1234@gmail.com>
2023-04-26 16:21:57 -0400

    added middleware

--- app.js
+++ app.js
@@ -2,29 +2,43 @@ require('dotenv');
 const colors = require('colors');
 const express = require('express');
 const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helment');
+const helmet = require('helmet');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const {localDb,prodDb} = require('./db/devices');
 
 const app = express();
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 //Routes
 const deviceRouter = require('./routes/devices.js');
 const oldRouter = require('./routes/oldDevices');
 const nelUsers = require('./routes/nelUsers');
 const deviceAge = require('./routes/deviceAge');
+const homePageRoute = require('./routes/home');
 
 app.use(express.json());
 
 prodDb();
 
+//Rate Limiting
+const limiter = rateLimit({
+    windowMs:10 * 60 * 1000, //10 minutes
+    max:100
+});
+
+app.use(limiter);
+
 //Sanitize Data
 app.use(mongoSanitize());
 
 //Set security headers
-app.use(helemt());
+app.use(helmet());
 
 //Prevent XSS attacksd
 app.use(xss());
@@ -35,6 +49,7 @@ app.use(hpp());
 //Enable CORS
 app.use(cors());
 
+app.use(homePageRoute)
 app.use('/api/current',deviceRouter);
 app.use('/api/old',oldRouter);
 app.use('/api/deviceage', deviceAge);
