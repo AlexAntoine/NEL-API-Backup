f671dd8ceff29c0aa4742abc450db53b329b5de4
Alex Antoine <antoinealex1234@gmail.com>
2023-05-04 14:50:32 -0400

    wrote excel to json conversion code

---
+++ test.js
@@ -0,0 +1,13 @@
+const xlsx = require('xlsx');
+const fs = require('fs');
+
+const wb = xlsx.readFile('./_data/nelDevicesAge_copy.xlsx', {dateaNF:'mm/dd/yyyy'});
+// console.log(wb.SheetNames);
+
+const ws = wb.Sheets["20211220 - ActiveDirectoryUsers"];
+// console.log(ws);
+
+const data = xlsx.utils.sheet_to_json(ws,{raw:false});
+// console.log(data);
+
+fs.writeFileSync('./_data/nelusers.json', JSON.stringify(data, null, 2));
