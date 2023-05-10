06e5defd05f2de3c5884311f00b750e9e8a7788a
Alex Antoine <antoinealex1234@gmail.com>
2023-05-10 15:08:42 -0400

    change local dn name

--- db/devices.js
+++ db/devices.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const localDb = async()=>{
     mongoose.set('strictQuery', true);
-    const connect = await mongoose.connect('mongodb://127.0.0.1:27017/FMLdb',{useNewUrlParser:true, useUnifiedTopology: true});
+    const connect = await mongoose.connect('mongodb://127.0.0.1:27017/fmlDb',{useNewUrlParser:true, useUnifiedTopology: true});
     console.log('Database Connected'.cyan.underline.bold);
 }
 
