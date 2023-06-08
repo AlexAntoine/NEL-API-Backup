e0ec60ba31d15b3c39bf5b30e1e0d36637eee5b7
Alex Antoine <antoinealex1234@gmail.com>
2023-06-08 15:57:49 -0400

    brought in aut middleware

--- routes/devices.js
+++ routes/devices.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth =  require('../middleware/auth');
 const {getCurrentDevices,getSingleCurrentDevice, deleteSingleCurrentDevice,addNewDevice, updateCurrentDevice} = require('../controller/devices');
 
 const router = express.Router();
 
-router.route('/').get(getCurrentDevices).post(addNewDevice);
+router.route('/').get(auth, getCurrentDevices).post(addNewDevice);
 router.route('/:id').get(getSingleCurrentDevice).delete(deleteSingleCurrentDevice).put(updateCurrentDevice);
 
 module.exports = router;
