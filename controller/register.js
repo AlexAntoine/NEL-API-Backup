e521d77979cd72dfa7387f60ea833d308055744c
Alex Antoine <antoinealex1234@gmail.com>
2023-06-08 15:56:48 -0400

    added create user function and login function

--- controller/register.js
+++ controller/register.js
@@ -1,3 +1,20 @@
-exports.login = (req, res)=>{
-    console.log(req.body);
+const User = require('../model/users');
+
+exports.createUser = async(req, res)=>{
+   const user = new User(req.body);
+   
+   const newUser = await user.save();
+
+   const token = await newUser.generateToken();
+
+   res.status(201).send({newUser,token});
+}
+
+exports.login = async(req, res)=>{  
+ 
+    const user = await User.findByCredentials(req.body.email,req.body.password);
+    const token = await user.generateToken();
+
+    res.send({user, token});
+
 }
