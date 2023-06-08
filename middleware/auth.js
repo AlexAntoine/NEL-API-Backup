3056308c14d4c5f685531933002276d6e4e2a482
Alex Antoine <antoinealex1234@gmail.com>
2023-06-08 15:57:10 -0400

    added auth middleware for protecting routes

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const User = require('../model/users');
+
+const auth = async(req, res, next)=>{
+
+    try {
+        const token = req.header('Authorization').replace('Bearer ','');
+
+        const decode = jwt.verify(token, 'abcd');
+    
+        const user = await User.findOne({_id:decode._id,'tokens.token':token});
+    
+        if(!user){
+            throw new Error();
+        }
+    
+        req.token = token;
+        req.user = user;
+    
+        next();
+        
+    } catch (error) {
+        res.status(401).send('Please Authenticate');
+    }
+
+  
+}
+
+module.exports = auth;
