80cfba431764f4fcca0a070006ad1548b50c3422
Alex Antoine <antoinealex1234@gmail.com>
2023-06-08 13:08:34 -0400

    added methods on schema

--- model/users.js
+++ model/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
 
@@ -16,6 +18,28 @@ const userSchema = new mongoose.Schema({
         token:{type:String, required:true} 
     }]
 });
+
+userSchema.pre('save',async function(next){
+    const user = this;
+
+    if(user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+userSchema.methods.generateToken = async function(){
+    const user = this;
+
+    const token = await jwt.sign({_id:user._id.toString()},'abcd');
+
+    user.tokens = user.tokens.concat({token});
+
+    await user.save();
+
+    return token;
+};
 
 const Users = mongoose.model('users',userSchema);
 
