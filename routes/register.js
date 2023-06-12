684239151703899d6d6f72a4daab2cc0939b1212
Alex Antoine <antoinealex1234@gmail.com>
2023-06-12 16:05:28 -0400

    added new route and method for web registration

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {createUser, login} = require('../controller/register');
+const {createUser, login, webRegister} = require('../controller/register');
 
 router.route('/register').post(createUser);
+router.route('/register/web').post(webRegister);
 router.route('/login').post(login);
 
 module.exports = router;
