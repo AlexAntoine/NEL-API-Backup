cd374d92efba69b39f1350857e3380a6228fa676
Alex Antoine <antoinealex1234@gmail.com>
2023-06-13 15:28:14 -0400

    added login method to route

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {createUser, login, webRegister} = require('../controller/register');
+const {createUser, login, webRegister, webLogin} = require('../controller/register');
 
 router.route('/register').post(createUser);
-router.route('/register/web').post(webRegister);
+router.route('/web/login').post(webLogin);
+router.route('/web/register').post(webRegister);
 router.route('/login').post(login);
 
 module.exports = router;
