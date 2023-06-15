c9b7f610f12e4aded090d28df26b6a439fe6f66f
Alex Antoine <antoinealex1234@gmail.com>
2023-06-14 23:52:50 -0400

    added auth got users route

--- routes/nelUsers.js
+++ routes/nelUsers.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const {getNelUsers, getSingleNelUser, deleteSingleNelUser,updateNelUsers, addNelUsers} = require('../controller/nelUsers');
 
 const router = express.Router();
 
-router.route('/').get(getNelUsers).post(addNelUsers);
+router.route('/').get(auth, getNelUsers).post(addNelUsers);
 router.route('/:id').get(getSingleNelUser).delete(deleteSingleNelUser).put(updateNelUsers);
 
 module.exports = router;
