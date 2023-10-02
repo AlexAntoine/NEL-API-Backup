3f482ea176f05df25f7417141fca297723fa8d73
Alex Antoine <antoinealex1234@gmail.com>
2023-10-02 14:07:04 -0400

    added pagination to current records

--- routes/currentRecords.js
+++ routes/currentRecords.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {getAllCurrentRecords, getSingleRecord, addNewRecord, deleteSingleRecord} = require('../controller/currentRecords');
+const CurrentRecords = require('../model/currentRecords');
+
+const advancedResult = require('../middleware/advancedResult');
 
 const router = express.Router();
 
-router.route('/').get(getAllCurrentRecords).post(addNewRecord);
+router.route('/').get(advancedResult(CurrentRecords),getAllCurrentRecords).post(addNewRecord);
 router.route('/:id').get(getSingleRecord).delete(deleteSingleRecord)
 
 module.exports = router;
