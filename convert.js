9050102dc01046eaa57aa83f2df003389c635c5e
Alex Antoine <antoinealex1234@gmail.com>
2023-10-17 14:11:16 -0400

    converted file and added new json data

--- convert.js
+++ convert.js
@@ -1,13 +1,13 @@
 const xlsx = require('xlsx');
 const fs = require('fs');
 
-const wb = xlsx.readFile('./_data/currentRecords.xlsx', {dateaNF:'mm/dd/yyyy'});
-console.log(wb.SheetNames);
+const wb = xlsx.readFile('./_data/nelDevicesAge.xlsx', {dateaNF:'mm/dd/yyyy'});
+// console.log(wb.SheetNames);
 
-const ws = wb.Sheets["CurrentRecords"];
+const ws = wb.Sheets["CurrentDevices"];
 // console.log(ws);
 
 const data = xlsx.utils.sheet_to_json(ws,{raw:false});
 // console.log(data);
 
-fs.writeFileSync('./_data/currentRecords.json', JSON.stringify(data, null, 2));
+fs.writeFileSync('./_data/currentDevices.json', JSON.stringify(data, null, 2));
