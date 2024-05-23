c11dbecbf28bbe522f43ab4cc40446b4108d2934
Alex Antoine <antoinealex1234@gmail.com>
2024-05-23 10:02:26 -0400

    testing

--- convert.js
+++ convert.js
@@ -1,15 +1,18 @@
+// Turn xlsx files into json 
 const xlsx = require('xlsx');
 const fs = require('fs');
 
-const wb = xlsx.readFile('./_data/nelDevicesAge.xlsx', {dateaNF:'mm/dd/yyyy'});
-// console.log(wb.SheetNames);
+const currentRecords = './_data/CurrentRecords.xlsx';
+const nelDevicesAge = './_data/nelDevicesAge.xlsx';
+const updatedDevices  = './_data/updated_devices.xlsx';
+
+const wb = xlsx.readFile(updatedDevices, {dateaNF:'mm/dd/yyyy'});
+console.log(wb.SheetNames);
 
 const ws = wb.Sheets["CurrentDevices"];
 // console.log(ws);
 
 const data = xlsx.utils.sheet_to_json(ws,{raw:false});
-console.log(data);
+// console.log(data);
 
-
-
-// fs.writeFileSync('./_data/currentDevices.json', JSON.stringify(data, null, 2));
+fs.writeFileSync('./_data/json/currentDevices.json', JSON.stringify(data, null, 2));
